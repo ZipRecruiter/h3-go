fc37028094804ce1fda3f08134581f665466cce5
Ian Dees <iand@ziprecruiter.com>
2025-01-06 18:18:56 -0600

    Add GridDisk for a CellSet

--- pkg/h3/cell_set.go
+++ pkg/h3/cell_set.go
@@ -36,6 +36,10 @@ func (cs CellSet) Cells() []Cell {
 	return cells
 }
 
+func (cs CellSet) String() string {
+	return fmt.Sprintf("%v", cs.Cells())
+}
+
 // Strings returns the cells in the set as a list of hex-encoded strings.
 func (cs CellSet) Strings() []string {
 	ss := make([]string, 0, len(cs))
@@ -54,4 +58,39 @@ func (cs CellSet) Contains(c Cell) bool {
 // Add adds a cell to the set.
 func (cs CellSet) Add(c Cell) {
 	cs[c] = struct{}{}
+}
+
+// Union returns the union of two cell sets.
+func (cs CellSet) Union(other CellSet) CellSet {
+	newSet := make(CellSet, len(cs)+len(other))
+	for c := range cs {
+		newSet[c] = struct{}{}
+	}
+	for c := range other {
+		newSet[c] = struct{}{}
+	}
+	return newSet
+}
+
+// GridDisk returns the cells in the set's grid disk of radius k. The grid disk
+// is the set of cells within k grid steps of the cells in the set. k=0 returns
+// the set itself.
+func (cs CellSet) GridDisk(k int) (CellSet, error) {
+	if k == 0 {
+		return cs, nil
+	}
+
+	newSet := make(CellSet)
+	for cell := range cs {
+		gridDiskListForCell, err := cell.gridDisk(k)
+		if err != nil {
+			return nil, fmt.Errorf("error computing grid disk for cell %s: %w", cell, err)
+		}
+
+		gridDiskSetForCell := NewCellSetFromCells(gridDiskListForCell)
+
+		newSet = newSet.Union(gridDiskSetForCell)
+	}
+
+	return newSet, nil
 }
